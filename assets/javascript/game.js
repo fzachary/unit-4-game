dc3fbd319f9352cbd24e16c422ea5cfb9af40a30
Frank Zachary <fzachary@gmail.com>
2019-07-21 11:54:21 -0400

    added reset and javascript files

---
+++ assets/javascript/game.js
@@ -0,0 +1,25 @@
+// WHEN PAGE LOADS
+
+    // Set the *win counter* and *loss counter* to 0 (variables)
+    // Run the RESET FUNCTION()
+
+// DEFINE RESET FUNCTION() FIRST {
+    // Reset *player's score* to 0 (variable for player's score)
+    // Reset the corresponding HTML for *player's score* in the DOM
+    // Generate a *target number* (VARIABLE) that the user is supposed to reach. The user must reach this number exactly. If they go over, they lose. Use Math.random to select a random number between 19 and 120.
+    // Console.log this number to check if working
+    // Generate a random number for each of the crystal images (divs) that will add to the *player's score* when clicked. ***This will require 4 variables.***
+    // Console.log these numbers to check if working.
+    // JQuery that applies the values to their respective images and adds to the *player's score* when clicked
+    // DO NOT reset the *win counter* and *loss counter* (both variables that will accrue values as the game is played more
+
+// }
+
+// GAME FUNCTION () {
+
+    // *ON CLICK EVENT FUNCTIONS*
+    // FUNCTION: When user clicks on images, add the assigned amount of points from each to the *player's score* variable + update the HTML with the current *player's score*
+    // COMPARE FUNCTION: If the *player's score* is below the *target number* do nothing but continue to run the game function
+    // If the *player's score* is above the *target number* then it is a loss. Add 1 to the *loss counter* variable. Update this in the DOM and RESET() the game (keeping the new *win counter*)
+    // If the *player's score* is equal to the *target number* then it is a win. Add 1 to the *win counter* variable. Update this in the DOM and RESET() the game (keeping the new *win counter*)
+
